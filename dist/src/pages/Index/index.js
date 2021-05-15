884fe9d1b8f117de80408eb50d97a8d83df79744
simplex1994 <1185482929@qq.com>
2021-05-15 21:23:05 +0800

    最小打包工具

--- dist/src/pages/Index/index.js
+++
@@ -1,6 +0,0 @@
-function Index() {
-  return /*#__PURE__*/React.createElement("div", null, "Hello World!");
-}
-
-window.Index = Index;
-ReactDOM.render(React.createElement(Index, {}, {}), document.getElementById('root'));
